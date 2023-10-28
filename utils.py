e33c4a80bac80ab9ccf9b21b5858c89fba1e27e2
Aidan Ewart <aidanprattewart@protonmail.com>
2023-10-28 20:05:07 +0100

    initial commit

--- utils.py
+++ utils.py
@@ -5,15 +5,27 @@ import sys
 from pathlib import Path
 from typing import List, Optional, Union
 
-import boto3
-from botocore.exceptions import ClientError, NoCredentialsError
+def load_vastnode_info():
+    """Load the vastnode info from the json file."""
+    with open("secrets/.vastnode", "r") as f:
+        vastnode_info = json.load(f)
+    return vastnode_info
 
-VAST_NUM = 4
+VAST_NUM = 0
 DEST_ADDR = f"root@ssh{VAST_NUM}.vast.ai"
-# DEST_ADDR = "mchorse@198.176.96.64"
 SSH_PYTHON = "/opt/conda/bin/python"
+PORT = 0
 
-PORT = 14040
+def init_vastnode():
+    """Initialise the vastnode."""
+    global VAST_NUM
+    global DEST_ADDR
+    global PORT
+
+    vastnode_info = load_vastnode_info()
+    VAST_NUM = vastnode_info["vast_num"]
+    PORT = vastnode_info["port"]
+    DEST_ADDR = f"root@ssh{VAST_NUM}.vast.ai"
 
 USER = "aidan"
 
@@ -32,6 +44,10 @@ def sync():
     command = f'rsync -rv --filter ":- .gitignore" --exclude ".git" -e "ssh -p {PORT}" . {DEST_ADDR}:{SSH_DIRECTORY}'
     subprocess.call(command, shell=True)
 
+def connect():
+    """SSH into the remote host."""
+    command = f"ssh -p {PORT} {DEST_ADDR}"
+    subprocess.call(command, shell=True)
 
 def datasets_sync():
     """Sync .csv files with the remote host."""
@@ -57,7 +73,7 @@ def copy_models():
 
 def copy_secrets():
     """Copy the secrets.json file from local directory to the remote host."""
-    command = f"scp -P {PORT} secrets.json {DEST_ADDR}:{SSH_DIRECTORY}"
+    command = f'rsync -rv -e "ssh -p {PORT}" ./secrets {DEST_ADDR}:{SSH_DIRECTORY}'
     subprocess.call(command, shell=True)
 
 
@@ -87,12 +103,9 @@ def setup():
     sync()
     copy_models()
     copy_secrets()
-    command = f'ssh -p {PORT} {DEST_ADDR} "cd {SSH_DIRECTORY} && sudo apt -y install python3.9 python3.9-venv && python3.9 -m venv .env --system-site-packages && source .env/bin/activate && pip install -r requirements.txt" && apt install vim'
+    command = f'ssh -p {PORT} {DEST_ADDR} "cd {SSH_DIRECTORY} && sudo apt -y install python3.9 python3.9-venv && python3.9 -m venv .env --system-site-packages && source .env/bin/activate && pip install -r requirements.txt"'
     # command = f"ssh -p {VAST_PORT} {dest_addr} \"cd {SSH_DIRECTORY} && echo $PATH\""
     subprocess.call(command, shell=True)
-
-
-import warnings
 
 
 class dotdict(dict):
@@ -118,92 +131,13 @@ class dotdict(dict):
     def __delattr__(self, name):
         del self[name]
 
-
-def upload_to_aws(local_file_name) -> bool:
-    """ "
-    Upload a file to an S3 bucket
-    :param local_file_name: File to upload
-    :param s3_file_name: S3 object name. If not specified then local_file_name is used
-    """
-    secrets = json.load(open("secrets.json"))
-
-    s3 = boto3.client(
-        "s3",
-        aws_access_key_id=secrets["access_key"],
-        aws_secret_access_key=secrets["secret_key"],
-    )
-
-    if secrets["access_key"] in ACCESS_KEY_NAME_DICT:
-        name = ACCESS_KEY_NAME_DICT[secrets["access_key"]]
-    else:
-        name = "unknown"
-
-    s3_file_name = name + "-" + local_file_name
-
-    local_file_path = Path(local_file_name)
-    try:
-        if local_file_path.is_dir():
-            _upload_directory(local_file_name, s3)
-        else:
-            s3.upload_file(str(local_file_name), BUCKET_NAME, str(s3_file_name))
-        print(f"Upload Successful of {local_file_name}")
-        return True
-    except FileNotFoundError:
-        print(f"File {local_file_name} was not found")
-        return False
-    except NoCredentialsError:  # mypy: ignore, not sure why it doesn't think it's a valid exception class
-        print("Credentials not available")
-        return False
-
-
-def _upload_directory(path, s3_client):
-    for root, dirs, files in os.walk(path):
-        for file_name in files:
-            full_file_name = os.path.join(root, file_name)
-            s3_client.upload_file(str(full_file_name), BUCKET_NAME, str(full_file_name))
-
-
-def download_from_aws(files: Union[str, List[str]], force_redownload: bool = False) -> bool:
-    """
-    Download a file from an S3 bucket
-    :param files: List of files to download
-    :param force_redownload: If True, will download even if the file already exists
-
-    Returns:
-        True if all files were downloaded successfully, False otherwise
-    """
-    secrets = json.load(open("secrets.json"))
-    if isinstance(files, str):
-        files = [files]
-
-    if not force_redownload:
-        files = [f for f in files if not os.path.exists(f)]
-
-    s3 = boto3.client(
-        "s3",
-        aws_access_key_id=secrets["access_key"],
-        aws_secret_access_key=secrets["secret_key"],
-    )
-    all_correct = True
-    for filename in files:
-        try:
-            parent_dir = os.path.dirname(filename)
-            if not os.path.exists(parent_dir) and parent_dir != "":
-                os.makedirs(os.path.dirname(filename))
-            with open(filename, "wb") as f:
-                s3.download_fileobj(BUCKET_NAME, filename, f)
-
-            print(f"Successfully downloaded file: {filename}")
-        except ClientError:
-            print(f"File: {filename} does not exist")
-            all_correct = False
-
-    return all_correct
-
-
 if __name__ == "__main__":
+    init_vastnode()
+
     if sys.argv[1] == "sync":
         sync()
+    elif sys.argv[1] == "connect":
+        connect()
     elif sys.argv[1] == "models":
         copy_models()
     elif sys.argv[1] == "recent":
